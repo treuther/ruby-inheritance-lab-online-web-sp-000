a3e7338000b7abd5825691757d10997bad8ec488
Tracy Reuther <tracyreuther@gmail.com>
2019-07-07 21:32:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student  < User #This is a child (subclass)
 
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    #Takes in a string and adds that string to the
+    #student's @knowledge array
+    @knowledge << string
+  end
+  
+  def knowledge
+    #returns the student's knowledge array
+    @knowledge
+  end
+  
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User #This is the parent(super class)
+  
+  attr_accessor :first_name, :last_name
+  
+  
+end
